b0166a8e49dc2fe3aa603c4509c488580b2c328d
Dan Bergeland <dbergeland@pillartechnology.com>
2019-04-26 14:22:15 -0500

    get correct age label based on file name

--- src/image_batch_generator.py
+++ src/image_batch_generator.py
@@ -4,15 +4,15 @@ from os import listdir
 from os.path import isfile, join
 
 class ImageBatchGenerator():
-    def __init__(self, image_directory):
+    def __init__(self, image_directory, random_choice_generator):
         self.image_dir = image_directory
+        self.random_choice_generator = random_choice_generator
 
     def generate(self):
         path = '/Users/daniel.a.bergeland/Documents/ai/demographic_cam/test/test_images'
         files = [filename for filename in listdir(path) if isfile(join(path, filename))]
     
         while True:
-            image = files[0]
-            
-            with open(join(path, image), 'rb') as image:
-                yield misc.imread(image), 1
+            image_name = self.random_choice_generator(files)
+            with open(join(path, image_name), 'rb') as image:
+                yield misc.imread(image), int(image_name.split('_')[0])
--- test/image_batch_generator_test.py
+++ test/image_batch_generator_test.py
@@ -5,7 +5,9 @@ from src.image_batch_generator import ImageBatchGenerator
 class TestDataGenerator(unittest.TestCase):
     def setUp(self):
         self.target_path = 'test/test_images'
-        self.data_gen = ImageBatchGenerator(self.target_path)
+        self.random_choice = 0
+        choicer = lambda filelist: filelist[self.random_choice]
+        self.data_gen = ImageBatchGenerator(self.target_path, choicer)
 
     def test_getsImageDirectory(self):
         self.assertEqual(self.data_gen.image_dir, self.target_path)
@@ -14,6 +16,12 @@ class TestDataGenerator(unittest.TestCase):
         input, label = next(self.data_gen.generate())
         self.assertEqual(np.shape(input), (200, 200, 3))
         self.assertEqual(label, 1)
+
+
+    def test_callingYieldAndGettingSecondImageHasLabelOf31(self):
+        self.random_choice = 2
+        input, label = next(self.data_gen.generate())
+        self.assertEqual(label, 31)
 
 
 
