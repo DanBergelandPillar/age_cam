50abf29aa24bcc1482e7807ad52ef176c7120824
Dan Bergeland <dbergeland@pillartechnology.com>
2019-05-17 13:09:33 -0500

    adds jupyter notebook and model_builder files

---
+++ src/model_builder.py
@@ -0,0 +1,14 @@
+from src.model_builder import inception-age
+import unittest
+
+class TestModelBuilder(unittest.TestCase):
+    def setUp(self):
+        inception_model = 
+
+    def test_getsImageDirectory(self):
+        self.assertEqual(self.data_gen.image_dir, self.target_path)
+
+
+
+if '__name__' == '__main__':
+    unittest.main()
