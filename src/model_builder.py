bd154addb3c4090a128b1e5ac22f551558ac20d5
Dan Bergeland <dbergeland@pillartechnology.com>
2019-05-17 13:46:36 -0500

    adds Inception Model

--- src/model_builder.py
+++ src/model_builder.py
@@ -1,14 +1,18 @@
-from src.model_builder import inception-age
-import unittest
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras import Model
 
-class TestModelBuilder(unittest.TestCase):
-    def setUp(self):
-        inception_model = 
+def InceptionAge():
+    base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224,224,3))
+    # add a global spatial average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    # let's add a fully-connected layer
+    x = Dense(1024, activation='relu')(x)
+    # and a logistic layer
+    predictions = Dense(1, activation='softmax')(x)
 
-    def test_getsImageDirectory(self):
-        self.assertEqual(self.data_gen.image_dir, self.target_path)
+    # this is the model we will train
+    model = Model(inputs=base_model.input, outputs=predictions)
 
-
-
-if '__name__' == '__main__':
-    unittest.main()
+    return model
